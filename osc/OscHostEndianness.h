a477d63840acdceb4928725cbc7bb53dcc1626d1
Markus Iser <2.markus.iser@gmail.com>
2018-06-07 14:20:20 +0200

    system detection: turned error into warning, guess endiannes

--- osc/OscHostEndianness.h
+++ osc/OscHostEndianness.h
@@ -119,7 +119,10 @@
 
 #if !defined(OSC_HOST_LITTLE_ENDIAN) && !defined(OSC_HOST_BIG_ENDIAN)
 
-#error please edit OSCHostEndianness.h or define one of {OSC_HOST_LITTLE_ENDIAN, OSC_HOST_BIG_ENDIAN} to configure endianness
+//#error please edit OSCHostEndianness.h or define one of {OSC_HOST_LITTLE_ENDIAN, OSC_HOST_BIG_ENDIAN} to configure endianness
+#warning system detection failed, guessing endianness, picking little endian
+#define OSC_HOST_LITTLE_ENDIAN 1
+
 
 #endif
 
